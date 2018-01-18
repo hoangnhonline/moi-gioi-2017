7ab5dfd14a75f3e4031cfb0b877f4a6d11e045ff
HoangNH <mrhoang1989@gmail.com>
2018-01-18 10:04:56 +0700

    .

--- resources/views/backend/articles/edit.blade.php
+++ resources/views/backend/articles/edit.blade.php
@@ -9,7 +9,7 @@
     <ol class="breadcrumb">
       <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
       <li><a href="{{ route('articles.index') }}">Bài viết</a></li>
-      <li class="active"><span class="glyphicon glyphicon-pencil"></span></li>
+      <li class="active">Cập nhật</li>
     </ol>
   </section>
 
@@ -17,7 +17,24 @@
   <section class="content">
     <a class="btn btn-default btn-sm" href="{{ route('articles.index') }}" style="margin-bottom:5px">Quay lại</a>
     <a class="btn btn-primary btn-sm" href="{{ route('news-detail', [$detail->slug, $detail->id ]) }}" target="_blank" style="margin-top:-6px"><i class="fa fa-eye" aria-hidden="true"></i> Xem</a>
-    <form role="form" method="POST" action="{{ route('articles.update') }}">
+    <div class="block-author edit">
+      <ul>
+        <li>
+          <span>Tác giả:</span>
+          <span class="name">{!! $detail->createdUser->display_name !!}</span>
+        </li>
+        <li>
+            <span>Ngày tạo:</span>
+          <span class="name">{!! date('d/m/Y H:i', strtotime($detail->created_at)) !!}</span>
+          
+        </li>
+         <li>
+            <span>Cập nhật lần cuối:</span>
+          <span class="name">{!! $detail->updatedUser->display_name !!} ( {!! date('d/m/Y H:i', strtotime($detail->updated_at)) !!} )</span>          
+        </li>        
+      </ul>
+    </div>
+    <form role="form" method="POST" action="{{ route('articles.update') }}" id="dataForm">
     <div class="row">
       <!-- left column -->
       <input name="id" value="{{ $detail->id }}" type="hidden">
@@ -63,17 +80,15 @@
                 <span class=""></span>
                 <div class="form-group">                  
                   <label>Slug <span class="red-star">*</span></label>                  
-                  <input type="text" class="form-control" name="slug" id="slug" value="{{ $detail->slug }}">
+                  <input type="text" class="form-control"  readonly="readonly" name="slug" id="slug" value="{{ $detail->slug }}">
                 </div>
                 
                 <div class="form-group" style="margin-top:10px;margin-bottom:10px">  
                   <label class="col-md-3 row">Thumbnail ( 624x468 px)</label>    
                   <div class="col-md-9">
                     <img id="thumbnail_image" src="{{ $detail->image_url ? Helper::showImage($detail->image_url ) : URL::asset('public/admin/dist/img/img.png') }}" class="img-thumbnail" width="145" height="85">
-                    
-                    <input type="file" id="file-image" style="display:none" />
                  
-                    <button class="btn btn-default btn-sm" id="btnUploadImage" type="button"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> Upload</button>
+                    <button class="btn btn-default btn-sm btnSingleUpload" data-set="image_url" data-image="thumbnail_image" type="button"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> Upload</button>
                   </div>
                   <div style="clear:both"></div>
                 </div>
@@ -87,7 +102,7 @@
                   <div class="checkbox">
                     <label>
                       <input type="checkbox" name="is_hot" value="1" {{ $detail->is_hot == 1 ? "checked" : "" }}>
-                      Bài viết nổi bật
+                      HOT
                     </label>
                   </div>               
                 </div>
@@ -120,8 +135,7 @@
                 <input type="hidden" id="editor" value="content">
                   
             </div>          
-            <input type="hidden" name="image_url" id="image_url" value="{{ $detail->image_url }}"/>          
-            <input type="hidden" name="image_name" id="image_name" value="{{ $detail->image_name }}"/>
+            <input type="hidden" name="image_url" id="image_url" value="{{ $detail->image_url }}"/>
             <div class="box-footer">
               <button type="submit" class="btn btn-primary btn-sm">Lưu</button>
               <a class="btn btn-default btn-sm" class="btn btn-primary btn-sm" href="{{ route('articles.index')}}">Hủy</a>
@@ -169,7 +183,6 @@
   </section>
   <!-- /.content -->
 </div>
-<input type="hidden" id="route_upload_tmp_image" value="{{ route('image.tmp-upload') }}">
 <!-- Modal -->
 <div id="tagModal" class="modal fade" role="dialog">
   <div class="modal-dialog modal-lg">
@@ -203,155 +216,4 @@
 
   </div>
 </div>
-@stop
-@section('js')
-<script type="text/javascript">
-$(document).on('click', '#btnSaveTagAjax', function(){
-  $.ajax({
-    url : $('#formAjaxTag').attr('action'),
-    data: $('#formAjaxTag').serialize(),
-    type : "post", 
-    success : function(str_id){          
-      $('#btnCloseModalTag').click();
-      $.ajax({
-        url : "{{ route('tag.ajax-list') }}",
-        data: { 
-          type : 2 ,
-          tagSelected : $('#tags').val(),
-          str_id : str_id
-        },
-        type : "get", 
-        success : function(data){
-            $('#tags').html(data);
-            $('#tags').select2('refresh');
-            
-        }
-      });
-    }
-  });
-});
-  $(document).ready(function(){
-      $(".select2").select2();
-      var editor = CKEDITOR.replace( 'content',{
-          language : 'vi',
-          filebrowserBrowseUrl: "{{ URL::asset('/admin/dist/js/kcfinder/browse.php?type=files') }}",
-          filebrowserImageBrowseUrl: "{{ URL::asset('/admin/dist/js/kcfinder/browse.php?type=images') }}",
-          filebrowserFlashBrowseUrl: "{{ URL::asset('/admin/dist/js/kcfinder/browse.php?type=flash') }}",
-          filebrowserUploadUrl: "{{ URL::asset('/admin/dist/js/kcfinder/upload.php?type=files') }}",
-          filebrowserImageUploadUrl: "{{ URL::asset('/admin/dist/js/kcfinder/upload.php?type=images') }}",
-          filebrowserFlashUploadUrl: "{{ URL::asset('/admin/dist/js/kcfinder/upload.php?type=flash') }}",
-          height : 500
-      });
-      $('#btnUploadImage').click(function(){        
-        $('#file-image').click();
-      });  
-      $('#btnAddTag').click(function(){
-          $('#tagModal').modal('show');
-      });    
-      var files = "";
-      $('#file-image').change(function(e){
-         files = e.target.files;
-         
-         if(files != ''){
-           var dataForm = new FormData();        
-          $.each(files, function(key, value) {
-             dataForm.append('file', value);
-          });   
-          
-          dataForm.append('date_dir', 1);
-          dataForm.append('folder', 'tmp');
-
-          $.ajax({
-            url: $('#route_upload_tmp_image').val(),
-            type: "POST",
-            async: false,      
-            data: dataForm,
-            processData: false,
-            contentType: false,
-            success: function (response) {
-              if(response.image_path){
-                $('#thumbnail_image').attr('src',$('#upload_url').val() + response.image_path);
-                $( '#image_url' ).val( response.image_path );
-                $( '#image_name' ).val( response.image_name );
-              }
-              console.log(response.image_path);
-                //window.location.reload();
-            },
-            error: function(response){                             
-                var errors = response.responseJSON;
-                for (var key in errors) {
-                  
-                }
-                //$('#btnLoading').hide();
-                //$('#btnSave').show();
-            }
-          });
-        }
-      });
-      
-      
-      $('#title').change(function(){
-         var name = $.trim( $(this).val() );
-         if( name != '' && $('#slug').val() == ''){
-            $.ajax({
-              url: $('#route_get_slug').val(),
-              type: "POST",
-              async: false,      
-              data: {
-                str : name
-              },              
-              success: function (response) {
-                if( response.str ){                  
-                  $('#slug').val( response.str );
-                }                
-              },
-              error: function(response){                             
-                  var errors = response.responseJSON;
-                  for (var key in errors) {
-                    
-                  }
-                  //$('#btnLoading').hide();
-                  //$('#btnSave').show();
-              }
-            });
-         }
-      });
-      $('#parent_id').change(function(){
-        $.ajax({
-            url: $('#route_get_cate_by_parent').val(),
-            type: "POST",
-            async: false,
-            data: {          
-                parent_id : $(this).val(),
-                type : 'list'
-            },
-            success: function(data){
-                $('#cate_id').html(data).select2('refresh');                      
-            }
-        });
-      });
-      $('#btnLoadMovies').click(function(){
-        if( $('#url').val() != '' ){
-          $('#spanLoad').removeClass('glyphicon glyphicon-download-alt').addClass('fa fa-spin fa-spinner');
-          $.ajax({
-              url: $('#route_get_movies_external').val(),
-              type: "POST",
-              async: true,
-              data: {          
-                  url : $('#url').val()                
-              },              
-              success: function(response){      
-                  $('#title').val(response.title);
-                  $('#slug').val(response.slug);
-                  $('#thumbnail_image').attr('src', response.image_url);
-                  $('#image_url').val(response.image_url);                
-                  $('#spanLoad').removeClass('fa fa-spinner fa-spin').addClass('glyphicon glyphicon-download-alt');              
-                                      
-              }
-          });
-        }
-      });
-    });
-    
-</script>
 @stop
