4b62800c42d83669c7502f12d386976cb81d5f8b
HoangNH <mrhoang1989@gmail.com>
2018-01-18 10:40:47 +0700

    .

--- resources/views/frontend/news/index.blade.php
+++ resources/views/frontend/news/index.blade.php
@@ -5,50 +5,98 @@
   
 
 @section('content')
-<section class="col-sm-8 col-xs-12 block-sitemain">
+<style>
+    .title_sanpham {
+        text-transform: uppercase;
+        color: #1060EB;
+        font-size: 18px;
+        background: url(../../public/assets/images/icon_tintuc1.png) left no-repeat;
+        display: block;
+        text-indent: 35px;
+        float: left;
+        margin-bottom: 22px;
+    }
+
+    .khung_sanpham {
+        border: 1px solid rgba(204, 204, 204, 0.44);
+        background: #fff;
+        margin-bottom: 20px;
+    }
+</style>
+<style>
+    .xemtiep:hover {
+        color: #0C6 !important;
+        box-shadow: 0px 0px 20px -5px #AD63C3 inset;
+    }
+
+    .ten_tintuc1:hover {
+        color: #F00 !important;
+        transition: all 0.5s;
+    }
+
+    .img_hover:hover {
+        opacity: 0.5;
+    }
+
+    @media screen and (max-width: 650px) {
+        .div_img {
+            width: 100% !important;
+        }
+
+        .div_thongtin {
+            width: 100% !important;
+        }
+    }
+</style>
+<section class="row">
 <article class="block-breadcrumb-page">
     <ul class="breadcrumb"> 
         <li><a href="{{ route('home') }}" title="Trở về trang chủ">Trang chủ</a></li>            
         <li class="active">{!! $cateDetail->name !!}</li>
     </ul>
 </article>
-    <article class="block block-breadcrumb">
-      <div class="block-contents">
-        <ul>
-          <li class="active"><h2><a href="{{ route('news-list', $cateDetail->slug) }}">{!! $cateDetail->name !!}</a></h2></li>
-        </ul>
-      </div>
-    </article><!-- /block-breadcrumb -->    
-
-    <article class="block block-article-list clearfix">
-      <div class="col-sm-12 col-xs-12">
-        <div class="row">
-          <div class="block-contents">
-            <ul class="article-list-news">
-              @foreach( $articlesArr as $articles )
-              <li class="article-news-item">
-                <div class="article-news-item-head">
-                  <a id="" href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}"><img title="{!! $articles->title !!}" src="{{ Helper::showImageThumb($articles->image_url, 2) }}" alt="{!! $articles->title !!}"></a>
+</section>
+<p class="title_sanpham">TIN TỨC</p>
+<div class="clear"></div>
+<div class="khung_sanpham">
+    <div style="padding:20px 20px;">
+        @foreach( $articlesArr as $articles )        
+        <div style="margin-bottom:20px;border-bottom:1px dashed rgba(204, 204, 204, 0.43);padding-bottom:20px;">
+            <div class="div_img"
+                 style="width:40%;height:auto;float:left;vertical-align: middle;text-align: center;display: table-cell;">
+                <div class="hinhsp hinhsp_108">
+                    <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}"
+                       title="{!! $articles->title !!}"><img
+                                class="img_hover img_trung img_trung_108"
+                                src="{{ Helper::showImageThumb($articles->image_url, 2) }}"
+                                alt="{!! $articles->title !!}"
+                                style="width: 100%; height: 250.733px;"></a>
                 </div>
-                <div class="article-news-item-description">
-                  <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" title="{!! $articles->title !!}">{!! $articles->title !!}</a>
-                  <div class="nd-time">{{ date('d-m-Y', strtotime($articles->created_at)) }}  <small>{{ date('H:i', strtotime($articles->created_at)) }} </small></div>
-                  <p>{!! $articles->description !!}</p>
-                </div>
-              </li>
-              @endforeach
-             
-            </ul>
-            <div style="text-align:center">
-            {{ $articlesArr->links() }}
-            </div> 
-            </nav>
-          </div>
+                <div class="clear"></div>
+            </div>
+            <div class="div_thongtin" style="width:55%;height:auto;float:right;">
+                <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}"
+                   title="{!! $articles->title !!}"><p class="ten_tintuc1"
+                                                                           style="text-transform: uppercase;color: #1060EB;font-size: 15px;line-height: 25px;">
+                        {!! $articles->title !!}</p></a>
+                <p style="font-size: 16px;line-height:25px;color: #766E6E;margin:10px 0px;">{!! $articles->description !!}</p>
+                <a href="{{ route('news-detail', ['slug' => $articles->slug, 'id' => $articles->id]) }}" class="xemtiep"
+                   style="background:#1060EB;float:left;color:#FFF;font-size:12px;padding: 5px 10px;border-radius: 5px;">Xem
+                    tiếp</a>
+            </div>
+            <div class="clear"></div>
         </div>
-      </div>
-    </article><!-- /block-news-new -->
+        @endforeach
 
-  </section><!-- /block-site-left -->
+       
+
+        <div class="clear"></div>
+
+    </div>
+    <div class="clear"></div>
+</div>
+
+
 @endsection
   @section('javascript_page')
   
