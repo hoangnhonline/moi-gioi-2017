6cfd3c22806fc8ab6e3e6ce35ea718a9c9caa3ca
HoangNH <hoangnhonline@gmail.com>
2018-03-06 06:16:59 +0700

    .

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -58,35 +58,10 @@ class HomeController extends Controller
             }
         }
 
-        $productArr = [];
-        $hoverInfo = [];
+        $productArr = [];        
         $loaiSp = EstateType::where('status', 1)->get();
         $bannerArr = [];          
         $articlesArr = Articles::where(['cate_id' => 1])->orderBy('id', 'desc')->get();
-        $hotProduct = Product::where('product.slug', '<>', '')
-                    ->where('product.type', 1)
-                    ->where('product.status', 1)
-                    ->leftJoin('product_img', 'product_img.id', '=','product.thumbnail_id')            
-                    ->join('estate_type', 'estate_type.id', '=','product.estate_type_id')      
-                    ->select('product_img.image_url as image_urls', 'product.*', 'estate_type.slug as slug_loai')
-                    ->where('product_img.image_url', '<>', '')
-                      ->where('product.cart_status', 1)                                         
-                    ->orderBy('product.is_hot', 'desc')
-                    ->orderBy('product.cart_status', 'asc')                    
-                    ->orderBy('product.id', 'desc')->limit(5)->get();
-        $hotProduct2 = Product::where('product.slug', '<>', '')
-                    ->where('product.type', 2)
-                    ->where('product.status', 1)
-                    ->where('product.cart_status', 1)
-                    ->leftJoin('product_img', 'product_img.id', '=','product.thumbnail_id')            
-                    ->join('estate_type', 'estate_type.id', '=','product.estate_type_id')      
-                    ->select('product_img.image_url as image_urls', 'product.*', 'estate_type.slug as slug_loai')
-                    ->where('product_img.image_url', '<>', '')                                         
-                    ->orderBy('product.is_hot', 'desc')
-                    ->orderBy('product.cart_status', 'asc')                    
-                    ->orderBy('product.id', 'desc')                    
-                    ->limit(5)->get();
-        
         $settingArr = Settings::whereRaw('1')->lists('value', 'name');
         $seo = $settingArr;
         $seo['title'] = $settingArr['site_title'];
@@ -94,29 +69,22 @@ class HomeController extends Controller
         $seo['keywords'] = $settingArr['site_keywords'];
         $socialImage = $settingArr['banner'];
 
-        $tinThiTruong = Articles::where('cate_id', 7)->orderBy('id', 'desc')->limit(6)->get()->toArray();
-        $phongthuy = Articles::where('cate_id', 4)->orderBy('id', 'desc')->limit(6)->get()->toArray();
-        
-        $khonggiansong = Articles::where('cate_id', 1)->orderBy('id', 'desc')->limit(6)->get()->toArray();
-
-        $luat = Articles::where('cate_id', 5)->orderBy('id', 'desc')->limit(6)->get()->toArray();
-        $tuvan = Articles::where('cate_id', 6)->orderBy('id', 'desc')->limit(6)->get()->toArray();
-
-        $phantich = Articles::where('cate_id', 1)->orderBy('id', 'desc')->limit(6)->get()->toArray();
-        $videoList = Video::where('status', 1)->orderBy('display_order')->get();
-        $videoFirst = $videoList->first();     
-
         $estateTypeList = EstateType::where('status', 1)->get();
         foreach($estateTypeList as $estate_type){
-            $productArr[$estate_type->id] = Product::where('estate_type_id', $estate_type->id)->where('product.cart_status', 1)->leftJoin('product_img', 'product_img.id', '=','product.thumbnail_id')        
-                     ->join('estate_type', 'estate_type.id', '=','product.estate_type_id')                  
-                    ->select('product_img.image_url as image_url', 'product.*', 'estate_type.slug as slug_loai')->orderBy('is_hot', 'desc')->orderBy('id', 'desc')->limit('9')->get();
+            $productArr[$estate_type->id] = Product::where('estate_type_id', $estate_type->id)->where('product.cart_status', 1)
+                    ->leftJoin('product_img', 'product_img.id', '=','product.thumbnail_id')        
+                    ->join('estate_type', 'estate_type.id', '=','product.estate_type_id')                  
+                    ->select('product_img.image_url as image_url', 'product.*', 'estate_type.slug as slug_loai')
+                    ->orderBy('is_hot', 'desc')
+                    ->orderBy('id', 'desc')
+                    ->limit('9')
+                    ->get();
         }        
         $joinedProductArrId = [];
         if(Session::get('userId') > 0){
             $joinedProductArrId = Account::joinedProduct(Session::get('userId'));       
         }
-        return view('frontend.home.index', compact('bannerArr', 'articlesArr', 'socialImage', 'seo', 'countMess', 'hotProduct', 'tinThiTruong', 'luat', 'khonggiansong', 'phongthuy', 'tinRandom','hotProduct2', 'luat', 'tuvan', 'videoList', 'videoFirst', 'estateTypeList', 'productArr', 'joinedProductArrId'));
+        return view('frontend.home.index', compact('bannerArr', 'articlesArr', 'socialImage', 'seo', 'estateTypeList', 'productArr', 'joinedProductArrId'));
 
     }
 
@@ -142,15 +110,7 @@ class HomeController extends Controller
                         ->select('product_img.image_url', 'product.*', 'thuoc_tinh')
                         ->orderBy('id', 'desc')->paginate(20);
         $seo['title'] = $seo['description'] =$seo['keywords'] = "Tìm kiếm sản phẩm theo từ khóa '".$tu_khoa."'";
-        $hoverInfo = [];
-        if($productArr->count() > 0){
-            $hoverInfoTmp = HoverInfo::orderBy('display_order', 'asc')->orderBy('id', 'asc')->get();
-            foreach($hoverInfoTmp as $value){
-                $hoverInfo[$value->estate_type_id][] = $value;
-            }
-        }
-        //var_dump("<pre>", $hoverInfo);die;
-        return view('frontend.search.index', compact('productArr', 'tu_khoa', 'seo', 'hoverInfo'));
+        return view('frontend.search.index', compact('productArr', 'tu_khoa', 'seo'));
     }
     public function ajaxTab(Request $request){
         $table = $request->type ? $request->type : 'category';
